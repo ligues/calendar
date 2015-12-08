27eaf645ee143a108cf5925ac50b7e9c80a4e03c
fmanrique <paco@djnr.net>
2015-12-07 23:14:20 -0600

    correccion viewport

--- page.php
+++ page.php
@@ -8,7 +8,6 @@ function get_page() {
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=640">
 		<title></title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
